7657a63a1310e520e8ea2162de490c0a3f260052
EmonLe <130631183+EmonLe@users.noreply.github.com>
2023-05-07 18:57:27 +0800

    Update App.py

--- App.py
+++ App.py
@@ -15,14 +15,17 @@ db_conn = connections.Connection(
     user=customuser,
     password=custompass,
     db=customdb
-)
 
+)
 output = {}
 table = 'employee'
 
-
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    return render_template('frontpage.html')
+
+@app.route("/add-employee")
+def add_employee():
     return render_template('AddEmp.html')
 
 
@@ -47,11 +50,11 @@ def AddEmp():
         return "Please select a file"
 
     try:
+
         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
-
-        # Upload image file to S3 #
+        # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
 
@@ -76,74 +79,13 @@ def AddEmp():
 
     finally:
         cursor.close()
+        
 
-    print("all modifications done...")
+
+    print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
-@app.route("/getemp", methods=['GET', 'POST'])
-def GetEmp():
-    if request.method == 'POST':
-        emp_id = request.form['emp_id']
-        print(emp_id)
-
-        select_sql = "SELECT * FROM employee WHERE emp_id = %s"
-        cursor = db_conn.cursor()
-
-        try:
-            cursor.execute(select_sql, (emp_id,))
-            result = cursor.fetchall()
-
-            if not result:
-                return "Employee with ID: {} doesn't exist".format(emp_id)
-            else:
-                emp_data = result[0]
-                emp_name = emp_data[1] + " " + emp_data[2]
-                pri_skill = emp_data[3]
-                location = emp_data[4]
-
-                # Download image file from S3 #
-                emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-                s3 = boto3.resource('s3')
-
-                try:
-                    s3.Bucket(custombucket).download_file(emp_image_file_name_in_s3, emp_image_file_name_in_s3)
-                    print("Image file downloaded from S3...")
-
-                except Exception as e:
-                    return str(e)
-
-        finally:
-            cursor.close()
-
-        return render_template('GetEmpOutput.html', name=emp_name, pri_skill=pri_skill, location=location,
-                               emp_image_file_name_in_s3=emp_image_file_name_in_s3)
-
-    else:
-        return render_template('GetEmp.html')
-    
-@app.route("/getempoutput", methods=['GET', 'POST'])
-def GetEmpOutput():
-    cursor = db_conn.cursor()
-    select_sql = "SELECT * FROM employee"
-    cursor.execute(select_sql)
-    rows = cursor.fetchall()
-    emp_data = []
-    for row in rows:
-        emp_dict = {}
-        emp_dict['emp_id'] = row[0]
-        emp_dict['first_name'] = row[1]
-        emp_dict['last_name'] = row[2]
-        emp_dict['pri_skill'] = row[3]
-        emp_dict['location'] = row[4]
-        emp_dict['emp_image_url'] = "https://{0}.s3.{1}.amazonaws.com/emp-id-{2}_image_file".format(
-            bucket,
-            region,
-            str(row[0])
-        )
-        emp_data.append(emp_dict)
-
-    return render_template('GetEmpOutput.html', emp_data=emp_data)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+
